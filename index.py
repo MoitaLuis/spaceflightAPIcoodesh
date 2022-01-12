db35ebda5cf9d634669e8f285bd1a9ca1e062b14
MoitaLuis <luismoita1337@gmail.com>
2022-01-12 03:47:30 -0300

    adição endpoint deleção + rascunho dos 2 endpoints faltantes

--- index.py
+++ index.py
@@ -1,21 +1,41 @@
 from fastapi import FastAPI
 from requests.models import to_native_string
-from mongo import get_all_articles, get_article_id
-
+from mongo import get_all_articles, get_article_by_id, add_article, edit_article, delete_article
 
 app = FastAPI()
 
+# Retornar um Status: 200 e uma Mensagem "Back-end Challenge 2021 🏅 - Space Flight News"
 @app.get("/")
 def home():
     return {"Status": "200", "Mensagem": "Back-end Challenge 2021 🏅 - Space Flight News"}
 
+# Listar todos os artigos da base de dados, utilizar o sistema de paginação para não sobrecarregar a REQUEST
 @app.get("/articles")
 def articles():
     r = get_all_articles()
     return r
 
+# Obter a informação somente de um artigo
 @app.get("/articles/{id}")
 def articles_id(id: int):
-    r = get_article_id(id)
+    r = get_article_by_id(id)
     r["_id"] = str(r["_id"])
     return r
+""""
+@app.post("/new-article")
+def articles_post(article):
+    print(article)
+    add_article(article)
+    return {"Status": "200", "Mensagem": "Artigo adicionado"}
+
+# Atualizar um artigo baseado no id
+@app.put("/articles/{id}")
+def articles_put(id: int, article):
+    edit_article(id, article)
+    return {"Status": "200", "Mensagem": "Artigo editado"}
+"""
+# Remover um artigo baseado no id
+@app.delete("/delete-article/{id}")
+def articles_delete(id: int):
+    delete_article(id)
+    return {"Status": "200", "Mensagem": "Artigo deletado"}
--- mongo.py
+++ mongo.py
@@ -3,16 +3,12 @@ import json
 import requests
 
 response_API = requests.get('https://api.spaceflightnewsapi.net/v3/articles')
-#print(response_API.status_code)
-parse_json = json.loads(response_API.text)
-#print(parse_json)
-
 cluster = MongoClient("mongodb+srv://admin:admin@codeshchallenge.q5tyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
 db = cluster["CodeshChallenge"]
 collection = db["CodeshChallenge"]
 
 
+parse_json = json.loads(response_API.text)
 for i in parse_json:
     #verificar se id ja esta no banco
     if collection.find_one({"id": i["id"]}):
@@ -21,13 +17,15 @@ for i in parse_json:
         collection.insert_one(i)
         print("id nao existe")
 
+# retorna todos os artigos
 def get_all_articles():
     r = collection.find({}).limit(5)
     for i in r:
         print(i["id"], i["title"])
     return r
 
-def get_article_id(id):
+# retorna um artigo com base no id
+def get_article_by_id(id):
     print(id)
     r = collection.find_one({"id":id})
     print(r)
@@ -38,4 +36,18 @@ def get_article_id(id):
     else:
         return {"Status": "404", "Mensagem": "Not Found"}
 
+# adiciona um artigo
+def add_article(article):
+    print(article)
+    collection.insert_one(article)
+    print("Artigo adicionado")
 
+# edita um artigo
+def edit_article(id, article):
+    collection.update_one({"id":id}, {"$set": article})
+    print("Artigo editado")
+
+# deleta um artigo
+def delete_article(id):
+    collection.delete_one({"id":id})
+    print("Artigo deletado")
